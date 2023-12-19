f089d5ae2e9b08bca61faf1427099ca63e00de8f
John Bitancor <john.bitancor@pygoamerica.com>
2023-12-18 20:19:30 -0700

    fixed hitbox

--- character.gd
+++ character.gd
@@ -63,37 +63,11 @@ func _physics_process(delta):
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
-	
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity.y -= gravity * delta
-#
-	## Handle jump.
-	##var jump = false
-	##if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		##velocity.y = JUMP_VELOCITY
-		##jump = true
-		#
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#print(direction)
-	##if (not jump and is_on_floor()):
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-#
-	#move_and_slide()
 
 func update_health():
 	var player_health = $HealthBar
 	player_health.value = HEALTH
 
 func _on_hitbox_area_entered(area):
-	if area.is_in_group("enemy"):
-		print("get shit on")
+	if area.is_in_group("enemies"):
+		print("halo")
