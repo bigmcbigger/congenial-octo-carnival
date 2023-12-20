6b473f57aea100df6ee20a1e6158f196e78109d6
John Kalyan <john.r.kalyan@gmail.com>
2023-12-19 18:54:45 -0700

    add player tracking to archer golem

--- archer.gd
+++ archer.gd
@@ -1,14 +1,32 @@
 extends CharacterBody3D
 
 # TODO set target on enter leash zone
-@onready var navigation_agent = $NavigationAgent3D
+var navigation_agent = null
+var navmap_ready = false
+var player = null
 
 func _ready():
 	call_deferred("deferred")
+	player = get_tree().current_scene.get_node("Player")
+	navigation_agent = $navigation_point.get_node("NavigationAgent3D")
+	navigation_agent.target_desired_distance = 2.0;
+	navigation_agent.path_desired_distance = 2.0;
 
 func deferred():
 	await get_tree().physics_frame
-	navigation_agent.set_target_position(Vector3(0,0,0))
+	navmap_ready = true
+	
+func _process(delta):
+	if navmap_ready:
+		if not navigation_agent.is_navigation_finished():
+			# currently pathing, TODO update target if needed
+			if navigation_agent.target_position.distance_to(player.global_position) > 2.0:
+				navigation_agent.set_target_position(player.global_position)
+		else:
+			# need target, TODO set valid target if needed
+			# TODO can ignore y position
+			navigation_agent.set_target_position(player.global_position)
+			pass
 
 func _physics_process(delta):
 	if navigation_agent.is_navigation_finished():
@@ -17,5 +35,6 @@ func _physics_process(delta):
 	var current_position = global_position
 	var next_path_position = navigation_agent.get_next_path_position()
 
-	velocity = current_position.direction_to(next_path_position) * 10.0
+	velocity = current_position.direction_to(next_path_position) * 8.0;
+	velocity.y = 0.0;
 	move_and_slide()
