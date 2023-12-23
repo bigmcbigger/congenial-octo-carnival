6ab2d389b05f18e3221f7a045f678f5d9220b247
John Kalyan <john.r.kalyan@gmail.com>
2023-12-23 16:12:25 -0700

    add plan, cape material & tweak menu

--- menu.gd
+++ menu.gd
@@ -7,7 +7,13 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+var size = null
 func _process(delta):
+	# TODO figure out the right way to do this :D
+	var new_size = get_viewport().size
+	if size != new_size:
+		size = new_size;
+		$CenterContainer.set_size(size)
 	pass
 
 func start_pressed():
